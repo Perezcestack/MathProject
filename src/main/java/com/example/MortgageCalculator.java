2b091ef292b5027fdc462efec8532fc6c6f428aa
Cesar <Perezce41@gmail.com>
2024-09-30 15:40:50 -0700

    second commit

--- src/main/java/com/example/MortgageCalculator.java
+++ src/main/java/com/example/MortgageCalculator.java
@@ -1,22 +1,33 @@
 package com.example;
+
 import java.util.Scanner;
 
 public class MortgageCalculator {
     static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-    double interestRate;
-    int loanLength;
-    double loan;
+        double interestRate;
+        int loanLength;
+        double loan;
+        double monthlyPayment;
+
 
         System.out.print("Hi, Please enter your Interest rate: ");
-         interestRate = input.nextDouble();
+        interestRate = input.nextDouble();
+        double monthlyInterestRate = interestRate / 100 / 12;
 
         System.out.print("Please enter your loan length: ");
         loanLength = input.nextInt();
+        int totalPayments = loanLength * 12;
 
         System.out.print("Now please enter your loan amount: ");
         loan = input.nextDouble();
 
+        // calculate loan
+        monthlyPayment = loan * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments)) /
+                (Math.pow(1 + monthlyInterestRate, totalPayments) - 1);
+
+        System.out.println(monthlyPayment);
     }
 }
