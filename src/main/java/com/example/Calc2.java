4597b70ddda7ad3d3c8b6448c3e5d9bbd29d03b5
Cesar <Perezce41@gmail.com>
2024-10-01 09:23:53 -0700

    finished

--- src/main/java/com/example/Calc2.java
+++ src/main/java/com/example/Calc2.java
@@ -13,10 +13,16 @@ public class Calc2 {
         deposit = input.nextDouble();
 
         System.out.print("Now enter your interest rate: ");
-        interestRate = input.nextDouble();
+        interestRate = input.nextDouble() / 100;
 
         System.out.print("Now enter your loan length in years: ");
         years = input.nextInt();
+
+        //Calculations
+        double finalValue = deposit * Math.pow(1 + interestRate / 12,12 * years);
+        //double monthlyPayments = deposit * Math.pow(1 + interestRate / 12, years);
+
+        System.out.println("Your estimated worth would be " + finalValue + " and accrued interest is " + (finalValue - deposit));
 
 
 
--- src/main/java/com/example/Calculator2.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-import java.util.Scanner;
-
-public class Calculator2 {
-    public static void main(String[] args) {
-
-    }
-}
