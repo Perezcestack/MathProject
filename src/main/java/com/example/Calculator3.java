0a9a044545f1426370dc3a7939fef024704affd2
Cesar <Perezce41@gmail.com>
2024-10-01 09:28:56 -0700

    first commit

--- src/main/java/com/example/Calculator3.java
+++ src/main/java/com/example/Calculator3.java
@@ -1,4 +1,24 @@
 package com.example;
+import java.util.Scanner;
 
 public class Calculator3 {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        double monthlyPay;
+        double expectedInt;
+        int years;
+
+            // Asking user for inputs
+        System.out.print("Hello Please enter your monthly payout: ");
+        monthlyPay = input.nextDouble();
+
+        System.out.print("Now enter your expected interest: ");
+        expectedInt = input.nextDouble();
+
+        System.out.print("Enter your years to pay: ");
+        years = input.nextInt();
+
+
+
+    }
 }
