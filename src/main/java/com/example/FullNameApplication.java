ef7d2d31479109120e7c5ed171ef11671749c681
Cesar <Perezce41@gmail.com>
2024-10-01 15:57:21 -0700

    another assignment done

--- src/main/java/com/example/FullNameApplication.java
+++ src/main/java/com/example/FullNameApplication.java
@@ -2,8 +2,38 @@ package com.example;
 import java.util.Scanner;
 
 public class FullNameApplication {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+        String firstName;
+        String middleName;
+        String lastName;
+        String suffix;
+
+        System.out.println("Welcome to the Application software!");
+
         //Get names
+        System.out.print("Please enter your first name: ");
+        firstName = input.nextLine();
+
+        System.out.println("Please enter your middle name: (leave empty if N/A) ");
+        middleName = input.nextLine();
+
+
+        System.out.print("Please enter your last name: ");
+        lastName = input.nextLine();
+
+        System.out.print("Please enter your Suffix if any!: ");
+        suffix = input.nextLine();
+
+        firstName = firstName.trim();
+        middleName = middleName.trim();
+        lastName = lastName.trim();
+        suffix = suffix.trim();
+
+        String finalName = firstName + middleName + lastName + suffix;
+
+        System.out.println("Your name is " + finalName);
+
         //omit any spaces they  entered
         //return full name with comma before suffix
 
